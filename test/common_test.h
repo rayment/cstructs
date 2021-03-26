dc84d2b8c595400250d0aac02e0fdadc055a4f27
Finn Rayment <finn@rayment.fr>
2021-03-26 11:19:22 +0100

    test: Added ANSI-escape colouring to test suite and added iterator tests.

--- test/common_test.h
+++ test/common_test.h
@@ -13,18 +13,25 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define RED    "\x1b[1;31m"
+#define YELLOW "\x1b[1;33m"
+#define GREEN  "\x1b[1;32m"
+#define BOLD  "\x1b[1m"
+#define RESET  "\x1b[0m"
+
 #define INIT() clock_t tic, toc;
 
 #define FAIL(x,y) if (!(x)) {\
-                      fprintf(stdout, "\rFAILURE: %s\n", y);\
+                      fprintf(stdout, "\r" RED "FAILURE: " RESET "%s\n", y);\
                       return EXIT_FAILURE;\
                   } else {\
-                      fprintf(stdout, "\rSUCCESS: %s\n", y);\
+                      fprintf(stdout, "\r" GREEN "SUCCESS: " RESET "%s\n", y);\
                   }
 #define TEST(x,y,z) fprintf(stdout, "TEST: %s", z); fflush(stdout); x FAIL(y,z)
 
 #define TIME(x,y) tic = clock(); x toc = clock(); \
-                  fprintf(stdout, "BENCHMARK (%s): %fs\n", y, \
+                  fprintf(stdout, YELLOW "BENCHMARK " RESET \
+                          "(%s): " BOLD "%fs\n" RESET, y, \
                           (double) (toc - tic) / CLOCKS_PER_SEC);
 
 #endif /* _RAYMENT_FR_CSTRUCTS_TEST_COMMON_TEST_H */
